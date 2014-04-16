52909cd26ea63b5ac5fcad916ee839edbda77417
Josh Zhu <joshzhu@faimdata.com>
2014-04-15 22:39:02 -0700

    add group_state2.py

--- group_state2.py
+++ group_state2.py
@@ -20,30 +20,46 @@ def write_csv (csv_file, list_of_list):
   input and output setting
 '''
 
-path = '../cs341_2014/data'
+path = '../../Downloads/'
 data_file_name = 'njprod-beacon-201_beacon.log.2014-04-06'
 # data_file_name = 'small.txt'
-schema_file_name = 'schema'
+#schema_file_name = 'schema'
 output_file_name = 'us_data.csv'
 
 
 data_file = os.path.join (path, data_file_name)
-schema_file = os.path.join (path, schema_file_name)
+#schema_file = os.path.join (path, schema_file_name)
 output_file = os.path.join (path, output_file_name)
 
 
-schema_map = create_schema (schema_file)
+#schema_map = create_schema (schema_file)
 #print schema_map
 
 output_data = []
+cookie = []
+ip_address = []
+identifier = []
 with open (data_file) as f:
     for line in f:
         line = line.strip ()
         attr_list = line.split (',')
-        print len (attr_list)
         if (len(attr_list) == 60):
+            cookie.append(attr_list[22])
+            temp = attr_list[22].split('_')
+            #print(temp)
+            #print len(output_data)
+            if (len(temp) == 5):
+                ip = temp[0] + "." + temp[1] + "." + temp[2] + "." + temp[3] 
+                ip_address.append(ip)
+                identifier.append(temp[-1])
+            if (len(temp) == 1):
+                ip_address.append(temp)
+                identifier.append("null")
+            attr_list.append(ip_address[-1])
+            attr_list.append(identifier[-1])
             output_data.append(attr_list)
 
+#print identifier
 write_csv (output_file, output_data)
 print len (output_data)
 
