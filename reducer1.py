688abe42880ad8b2ed8ebb7816dbe490dad272c8
Josh Zhu <joshzhu@faimdata.com>
2014-04-16 14:25:30 -0700

    modify

--- reducer1.py
+++ reducer1.py
@@ -11,29 +11,54 @@ def read_mapper_output(file, separator='\t'):
         yield line.rstrip().split(separator, 1)
  
 def main(separator='\t'):
-    # input comes from STDIN (standard input)
-        
-    data = read_mapper_output(sys.stdin, separator=separator)
+    #filename = 'test.txt'
+    #data = read_mapper_output(filename, '\t')
+    #data = read_mapper_output(sys.stdin, separator=separator)
     # groupby groups multiple word-count pairs by word,
     # and creates an iterator that returns consecutive keys and their group:
     #   current_word - string containing a word (the key)
     #   group - iterator yielding all ["<current_word>", "<count>"] items
-    for identifier, group in groupby(data, itemgetter(0)):
-        try:
+    last_key = None
+    running_features = []
+    
+    #with open (filename) as f:
+        #for input_line in f:
+   for input_line in sys.stdin:
+           input_line = input_line.strip()
+           
+           this_key, value = input_line.split("\t", 1)
+           
+           if last_key == this_key:
+               running_features.append(value)
+        
+           else:
+                   if last_key:
+                       scores = []
+                       #print(running_features)
+                       for x in range(len(running_features)):
+                           for y in range(x+1, len(running_features)):
+                               record1 = running_features[x].split(',')
+                               record2 = running_features[y].split(',')
+                               #print (record1)
+                               if len(record1) == len(record2):
+                                scores.append(cal_jaccard(record1, record2))
+                       print ("%s%s%s" % (last_key, separator, ",".join(str(v) for v in scores)))
+                       running_features = []
+                   
+                   running_features.append(value)
+                   
+                   last_key = this_key
+ 
+        if last_key == this_key:
             scores = []
-            for identifier, feature1 in group:
-                for feature2 in group:
-                    if feature1 == feature2:
-                        continue
-                    else:
-                        record1 = feature1.split(',')
-                        record2 = feature2.split(',')
-                        if len(record1) == len(record2):
-                            scores.append(cal_jaccard(record1, record2))
-            print ("%s%s%s" % (identifier, separator, ",".join(str(v) for v in scores)))           
-        except ValueError:
-            # count was not a number, so silently discard this item
-            pass
+            for x in range(len(running_features)):
+                for y in range(x+1, len(running_features)):
+                    record1 = running_features[x].split(',')
+                    record2 = running_features[y].split(',')
+                    if len(record1) == len(record2):
+                        scores.append(cal_jaccard(record1, record2))
+            print ("%s%s%s" % (last_key, separator, ",".join(str(v) for v in scores)))
+        
  
 if __name__ == "__main__":
     main()
@@ -49,30 +74,3 @@ def cal_jaccard (record1, record2):
     
     return num / denom
     
-
-#device_map = {}
-#ip_map = Counter ()
-#
-#
-#for record in data:
-#	cookie = record[22];
-#	attr_list = cookie.split('_')
-#	
-#	if (len(attr_list) == 5):
-#		ip_addr = attr_list[0] + '.' + attr_list[1] + '.' + attr_list[2] + '.' + attr_list[3]
-#		ip_map[ip_addr] += 1 
-#		if (attr_list[4] in device_map):
-#			device_map[attr_list[4]].append (record)
-#		else:
-#			record_list = []
-#			record_list.append (record)
-#			device_map[attr_list[4]] = record_list
-#	else:
-#		print attr_list
-#
-#print len (data), len (device_map), len (ip_map)
-#print device_map.values()[:10]
-# print max(device_map.values()), max(ip_map.values())
-
-
-# write_csv ('../data/interest_records.csv', record_list)
