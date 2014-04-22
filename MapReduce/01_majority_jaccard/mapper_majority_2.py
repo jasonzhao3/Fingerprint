b910b357e2de429c59847960f56d7bde5a25c5df
jason zhao <yzhao3@stanford.edu>
2014-04-22 00:16:17 -0700

    add folders

---
+++ MapReduce/01_majority_jaccard/mapper_majority_2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import sys
+
+PROFILE_IDX = [
+               1, # publisher_id - majority
+               2, # network_id - majority
+               3, # domain_id - majority
+               8, # is_not_yume_white_list  - ratio of true
+               20, # city_name - jaccard set
+               21, # census_DMA - majority
+               31, # service_provider_name  - majority
+               32, # key_value - jaccard set
+               #35, # requested_date - frequency within 4 hours
+               36, # ip_addr - jaccard set
+               43, # service_provider - jaccard set
+               45, # player_size - jaccard set
+               47, # page_fold - majority
+               49, # play_type - majority
+               53, # hid - majority
+               55, # is_on_premises - 1's ratio
+               ];
+attr_num = len(PROFILE_IDX);
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    l = line.split('\t')
+    # split the line into words
+    record_list = l[1].split('|')
+    if len(record_list) >= 30:
+      attr_count = []
+      for i in range(0, attr_num):
+          attr_count.append([])
+      for record in record_list:
+          attr_l = record.split(',')
+          attr_list = [attr_l[i] for i in PROFILE_IDX]
+          for i in range(0, len(attr_list)):
+            attr_count[i].append(attr_list[i])
+      attr_res = []
+      for attrs in attr_count:
+        m = dict()
+        for attr in attrs:
+            if attr not in m:
+                m[attr]  = 0
+            m[attr] += 1
+        maxNum = 0
+        maxItem = ''
+        for key,value in m.items():
+            if (value > maxNum):
+                maxNum = value;
+                maxItem = key
+        attr_res.append(maxItem)
+      print '%s%s%s' % (l[0], "\t", ','.join(attr_res))
+
---
+++ MapReduce/01_majority_jaccard/reducer_jaccard.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import division
+import sys
+
+def cal_jaccard (record1, record2):
+    num = 0
+    denom = 0    
+    for i in range(len(record1)):
+        if (record1[i].lower() != "null" or record2[i].lower() != "null") and (record1[i].lower() != "n/a" or record2[i].lower() != "n/a"): 
+            denom = denom +1
+            if record1[i] == record2[i]:
+                num = num + 1   
+    return num / denom
+ 
+def main(separator='\t'):
+    #filename = 'test.txt'
+    #data = read_mapper_output(filename, '\t')
+    #data = read_mapper_output(sys.stdin, separator=separator)
+    # groupby groups multiple word-count pairs by word,
+    # and creates an iterator that returns consecutive keys and their group:
+    #   current_word - string containing a word (the key)
+    #   group - iterator yielding all ["<current_word>", "<count>"] items
+   last_key = None
+   records = {} 
+    #with open (filename) as f:
+        #for input_line in f:
+   for input_line in sys.stdin:
+       input_line = input_line.strip()
+       if not input_line:
+           continue
+       this_key, value = input_line.split("\t", 1)
+       #print(this_key)
+       records[this_key] = value
+       if last_key:
+           for prev in records:
+               if (prev != this_key):
+                   profile1 = records[prev].split(',')
+                   profile2 = records[this_key].split(',')
+                   if len(profile1) == len(profile2):
+                       emitted_key = prev + "," + this_key
+                       score = cal_jaccard(profile1, profile2)
+                       if score >= 0.4:
+                           print ("%s%s%s" % (emitted_key, separator, str(score)))
+       last_key = this_key
+ 
+if __name__ == "__main__":
+    main()
+        
+
+    
