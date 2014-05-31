42e9ebfa66525a7df791b54c4e518426ad52285f
Josh Zhu <joshzhu@faimdata.com>
2014-05-30 23:15:32 -0700

    mst

---
+++ MapReduce/08_pipeline_delivery_point/02_lsh/mst_merge_pairs/mapper_mst.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+'''
+
+Parallel step:
+  output all identifiers (nodes)
+
+  reducer output: node_num  \t concat_node with ','
+'''
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    line = line.strip()
+    l = line.split('\t')
+    if float(l[1]) >= 0.6:
+        print '%d%s%s' % (1, "\t", l[0])
